d44d7129afc06f9682cc21118cd12ecd784d6815
agentnova <arjunaj2234@gmail.com>
2021-01-08 17:32:45 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -5,6 +5,7 @@ load_dotenv(find_dotenv())
 
 
 class cred():
+    BOT_NAME=os.getenv("BOT_NAME")
     BOT_TOKEN = os.getenv("BOT_TOKEN") #From botfather
     API_ID = os.getenv("API_ID")       #"Get this value from my.telegram.org! Please do not steal"
     API_HASH = os.getenv("API_HASH")   #"Get this value from my.telegram.org! Please do not steal"
