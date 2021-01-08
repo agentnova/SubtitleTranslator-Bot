6fd7fc4426c30168f978e6aac15c8fa03b80a33a
agentnova <arjunaj2234@gmail.com>
2021-01-08 16:51:03 +0530

    Create creds.py

---
+++ creds.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+
+class cred():
+    BOT_TOKEN = os.getenv("BOT_TOKEN") #From botfather
+    API_ID = os.getenv("API_ID")       #"Get this value from my.telegram.org! Please do not steal"
+    API_HASH = os.getenv("API_HASH")   #"Get this value from my.telegram.org! Please do not steal"
+    DB_URL = os.getenv("DB_URL")       #From Firebase database
